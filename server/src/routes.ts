3b50b6dcbc8385f3eb9fec193834891100471482
Alex Bianchi <alexssandro.bianchi@intelltech.com.br>
2020-06-06 15:51:35 -0300

    Primeiro commit

---
+++ server/src/routes.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+routes.post('/points', pointsController.create);
+// routes.get('/points/:id', pointsController.show);
+
+
+/**
+ * Nomes dos metodos utilidados para as controllers
+ * index: list
+ * show: exibir um únoco valor da lista
+ * create: criar
+ * update: atualizar
+ * delete: deleta
+ */
+export default routes;
---
+++ server/teste.js
@@ -0,0 +1,14 @@
+var arrayDate = [926996400000, 927687600000, 1000350000000, 1001473200000,1101261600000, 
+                1101866400000, 1102471200000, 1572922800000, 1575342000000, 9];
+
+var newArray = Object.values(arrayDate).sort((a, b) => a - b);
+
+console.log(newArray);
+                
+                
+                var array2 = [ 10, 9];
+                var newArray2 = Object.values(array2).sort((a, b) => a - b);
+                console.log(newArray2);
+                
+                //926996400000
+                //1101866400000
