fcfcef6ab6c5bdffac9abbb40e05909ead2549eb
Alex Bianchi <alexssandro.bianchi@intelltech.com.br>
2020-06-13 10:31:06 -0300

    Finalizado NLW

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -10,6 +10,7 @@ import './styles.css';
 
 import logo from '../../assets/logo.svg'
 import Modal from '../../components/ValidationModal';
+import Dropzone from '../../components/Dropzone';
 
 //sempre que se cria um estado para um [] ou um {} se precisa manualmente informar o tipo da varivel que sera armazena 
 interface Item {
@@ -43,6 +44,8 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
+    const [selectedFile, setSelectedFile] = useState<File>();
+    
     const [showModal, setShowModal] = useState(false);
 
     const history = useHistory();
@@ -122,16 +125,19 @@ const CreatePoint = () => {
         const city = selectedCity;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            latitude,
-            longitude,
-            uf,
-            city,
-            items
-        };
+        const data = new FormData();
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+      
+        if(selectedFile) {
+            data.append('image', selectedFile);
+        }
 
         setShowModal(true);
 
@@ -156,7 +162,7 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br /> ponto de coleta</h1>
-
+                <Dropzone onFileUploaded={setSelectedFile} />
                 <fieldset>
                     <legend>
                         <h2>Dados</h2>
